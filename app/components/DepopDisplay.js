f0de78a22b736ed464022a084d8e8b29100697c8
kevinforrestkeyes <kevinforrestkeyes@gmail.com>
2019-09-29 16:56:58 -0700

    properly pulling in scrape status for module status

--- app/components/DepopDisplay.js
+++ app/components/DepopDisplay.js
@@ -1,20 +1,60 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { getDepopProducts } from '../utils/api';
+import { 
+	getDepopProducts, 
+	getDepopScrapeInfo, 
+	getDepopScrapeStatus,
+	updateDepopProducts } from '../utils/api';
 
 export default class DepopDisplay extends React.Component {
 	constructor(props) {
 		super(props);
 
-		this.state = {}
+		this.state = {
+			lastScrape: '',
+			scrapeStatus: '',
+		}
 
 		this.updateProducts = this.updateProducts.bind(this);
+		this.updateScrapeStatus = this.updateScrapeStatus.bind(this);
+		this.startNewDepopScrape = this.startNewDepopScrape.bind(this);
 	}
 
 	componentDidMount() {
 		if (this.props.products.length === 0) {
 			this.updateProducts();
 		}
+		this.updateScrapeStatus();
+		setInterval(() => {
+			getDepopScrapeStatus()
+				.then((status) => {
+					const scrapeStatus = status.updateInProgress ? 'in-progress' : 'completed';
+					if (scrapeStatus !== this.state.scrapeStatus) this.updateScrapeStatus();
+				});
+		}, 10000);
+	}
+
+	updateScrapeStatus() {
+		getDepopScrapeInfo()
+			.then((data) => {
+				const scrapeStatus = data.status.updateInProgress ? 'in-progress' : 'completed';
+				const lastScrapeIndex = Object.keys(data.log).find(index => {
+					const log = data.log[index];
+					if ((log.request === 'update-depop-products') && (log.runEndTime !== null)) {
+						return true;
+					}
+				});
+				const lastScrape = data.log[lastScrapeIndex].runEndTime;
+				this.setState({
+					scrapeStatus,
+					lastScrape
+				})
+			});
+	}
+
+	startNewDepopScrape() {
+		updateDepopProducts()
+			.then(() => this.updateScrapeStatus());
 	}
 
 	updateProducts() {
@@ -34,16 +74,36 @@ export default class DepopDisplay extends React.Component {
 	}
 
 	render() {
+		const { scrapeStatus, lastScrape } = this.state;
+		const scrapeStatusLoaded = ((scrapeStatus.length > 0) && (lastScrape.length > 0));
+
 		return (
 			<div className="depop-display module">
 				<div className="module-inner">
 					<div className="module-heading">
 						<h3>depop</h3>
 					</div>
+					<div className="module-status">
+						{ scrapeStatusLoaded
+						? <>
+								<p>scrape status: <span className={scrapeStatus}>{scrapeStatus}</span></p>
+								<p>last scrape: {lastScrape}</p>
+								<button 
+									disabled={scrapeStatus === 'in-progress'} 
+									onClick={this.startNewDepopScrape}>
+									{scrapeStatus === 'in-progress'
+										? 'scrape in progress'
+										: 'new scrape'
+									}
+								</button>
+							</>
+						: <p>loading status...</p>
+						}
+					</div>
 					<div className="module-content">
 						{ this.props.products.length > 0
 							? <ProductTable products={this.props.products} />
-							: <p>loading...</p>
+							: <p>loading products...</p>
 						}
 					</div>
 				</div>
--- app/utils/api.js
+++ app/utils/api.js
@@ -1,10 +1,48 @@
+const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+
 export function getDepopProducts() {
 	// only temporary until this is live
-	const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 	const targetUrl = 'http://hidden-temple-17823.herokuapp.com/get-depop-products';
 	return fetch(proxyUrl+targetUrl)
 		.then((res) => res.json())
 		.then((products) => {
 			return products;
+		});
+}
+
+export function getDepopScrapeInfo() {
+	return Promise.all([
+		getDepopLogData(),
+		getDepopScrapeStatus()
+	]).then(([ log, status ]) => ({
+		log,
+		status
+	}));
+}
+
+export function getDepopScrapeStatus() {
+	const targetUrl = 'http://hidden-temple-17823.herokuapp.com/scrape-status';
+	return fetch(proxyUrl+targetUrl)
+		.then((res) => res.json())
+		.then((status) => {
+			return status;
+		});
+}
+
+function getDepopLogData() {
+	const targetUrl = 'http://hidden-temple-17823.herokuapp.com/get-log';
+	return fetch(proxyUrl+targetUrl)
+		.then((res) => res.json())
+		.then((log) => {
+			return log;
+		});
+}
+
+export function updateDepopProducts() {
+	const targetUrl = 'http://hidden-temple-17823.herokuapp.com/update-depop-products';
+	return fetch(proxyUrl+targetUrl)
+		.then((res) => res.json())
+		.then((res) => {
+			return res;
 		});
 }
